098e17a44ef6ae855ac73e80f03f1728653ff054
TatyPimentaLeal <73079371+TatianePimentaLeal@users.noreply.github.com>
2021-05-26 21:06:52 -0300

    Delete repository pokemon
    Repositorio deletado devido a inclusão de arquivos atualizados.

--- pokedex-pokemon/README.markdown
+++
@@ -1,5 +0,0 @@
-# Pokémon 2.0
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/tatianepimentaleal/pen/YzNweRK](https://codepen.io/tatianepimentaleal/pen/YzNweRK).
-
-
--- pokedex-pokemon/script.js
+++
@@ -1,9 +0,0 @@
-var primeiroPoke = prompt("Digite seu palpite! ")
-var resultado = "Pikachu"
-
-if (resultado == primeiroPoke) {
-  var resultado = primeiroPoke
-  document.write("<h2>Você acertou!</h2>")
-} else if (resultado != primeiroPoke) {
-  document.write("<h2>Que pena! Tente novamente</h2>")
-}
